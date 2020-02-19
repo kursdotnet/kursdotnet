04c9b535f5c9a094a95c7d5d5c92c13f0b7526df
blackdante <kamil.m.kielbasa@gmail.com>
2020-02-19 20:22:20 +0100

    Add deploy.sh and package.json

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run docs:build
+
+cd docs/.vuepress/dist
+
+echo 'kursdotnetcore.pl' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:kursdotnet/kursdotnet.github.io.git production
